313cf8c13441acf8a2c2a03c997f7262a9a9b72b
Wesley Ryder <wr140815@ohio.edu>
2020-01-30 17:10:08 -0500

    week 4

--- src/np.py
+++ src/np.py
@@ -23,15 +23,37 @@ Vector  = List[int]          # Vector containing type int
 def windows(win, axe) -> None:
 
     avgNP = (win == 1).sum(axis = axe)
-    print(avgNP)
-
-    avg = np.mean(avgNP, axis = 0)
 
     largest = np.amax(avgNP)
     minum   = np.amin(avgNP)
+    avg     = np.mean(avgNP, axis = 0)
 
-    print(largest)
-    print(minum)
+    mma = (largest, minum, avg)
+
+    return mma
+
+def rm_chr_noise(win, gw_name):
+
+    avgnp = (win == 1).sum(axis = 1)
+    noise = avgnp[avgnp >= 100]
+
+    win.drop((noise.index), inplace = True)
+
+    gw_name = np.delete(gw_name, noise.index, axis = 0)
+
+    return win, gw_name
+
+def rm_np_noise(win):
+
+    columns = win.columns
+    avgnp   = (win == 1).sum(axis = 0)
+    noise   = avgnp[avgnp == 0]
+
+    win.drop((noise.index), inplace = True, axis = 1)
+
+    np_name = win.columns
+
+    return win, np_name
 
 #******************************************************************************
 # Function:     partition()
@@ -77,6 +99,25 @@ def calc_percentiles(win, axis, divisor) -> Vector:
 
     return vals
 
+def display_avgs(win) -> None:
+
+    # Nuclear profile and genomic window variables. These vars are tuples
+    # containing the max, min and average for the win dataframe axis.
+    npMMA = windows(win, 1)
+    gwMMA = windows(win, 0)
+    gws   = win.shape[0]    # Number of genomic windows
+    nps   = win.shape[1]   # Number of nuclear profiles
+
+    # Display the number of NP and GW in the current dataframe
+    print("The number of Nuclear Profiles = ", nps)
+    print("The number of Genomic Windows  = ", gws)
+
+    # Display the NP and GW max, min and avg from a dataframe
+    print("Nuclear Profiles: Max {} Min {} Avg {}"
+            .format(npMMA[0], npMMA[1], npMMA[2]))
+    print("Genomic Windows: Max {} Min {} Avg {}"
+            .format(gwMMA[0], gwMMA[1], gwMMA[2]))
+
 #******************************************************************************
 # Function:     main()
 # Parameters:   None
@@ -88,25 +129,44 @@ if __name__ == '__main__':
     # Import genome data set
     df = pd.read_csv(FDIR, sep = '\t', low_memory=False)
 
-    win = df.iloc[:, 3:] # numpy matrix of data (first 3 cols are not NP's)
-    gws = win.shape[0]   # Number of genomic windows
-    nps = win.shape[1]   # Number of nuclear profiles
+    win      = df.iloc[:, 3:] # numpy matrix of data (first 3 cols are not NP's)
+    np_names = np.array(win.columns.values)
+    gw_name  = np.array(df.iloc[:, :3])
 
-    print("The number of Nuclear Profiles = ", nps)
-    print("The number of Genomic Windows  = ", gws)
+    # Remove chromisome noise
+    win, gw_name = rm_chr_noise(win, gw_name)
+
+    # Display avgs for the dataframe
+    display_avgs(win)
 
     # Estimated radial possition of a NP 1-5 where 1 - strongly apical,
     # 3 - neither apical or equatorial, and 5 - strongly equatorial.
-    #np_percentiles = calc_percentiles(win, 0, 5)
     NP_percentiles = calc_percentiles(win, 0, 5)
 
     # Compaction of each genomic window. Degree of compaction rating 1-10
     # (where 10 is most condensed and 1 is least condensed
     GW_percentiles = calc_percentiles(win, 1, 10)
 
+    chromosome = np.argwhere(gw_name[:, 0] == 'chr13')
+    chromosome = chromosome[:,0]
+
+    chrIndexBegin = np.argwhere(gw_name[chromosome, 2] >= 21700000)
+    chrIndexEnd   = np.argwhere(gw_name[chromosome, 1] <= 24100000)
+    result        = np.intersect1d(chromosome[chrIndexBegin], chromosome[chrIndexEnd])
+
+    gw_name = gw_name[result]
+    win = win.iloc[result, :]
+
+    win, np_name = rm_np_noise(win)
+
+    display_avgs(win)
+    '''
     # Displaying indicis for the NP's and GW's
     for i in NP_percentiles:
-        print(i)
+        #print(i) # Indices
+        print(np_names[i])
 
     for i in GW_percentiles:
-        print(i)
+        #print(i) # Indices
+        print(gw_name[i])
+    '''
